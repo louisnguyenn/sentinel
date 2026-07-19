178de1f619be396db3c1103a6047b764c1a1056d
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-19 15:10:54 -0400

    implement photoeye constructor

--- controller/src/photoeye.cpp
+++ controller/src/photoeye.cpp
@@ -1,8 +1,13 @@
 #include "photoeye.hpp"
 
+// constructor
+sentinel::Photoeye::Photoeye(double debounce_time_s = 0.01) : m_debounce_time_s{debounce_time_s}
+{
+}
+
 /// @brief state of the photoeye sensor (HIGH if object, LOW if not object present)
-/// @param dt_s 
-/// @param part_physically_present 
+/// @param dt_s
+/// @param part_physically_present
 void sentinel::Photoeye::update(double dt_s, bool part_physically_present)
 {
     if (part_physically_present != m_pending_state)
@@ -14,7 +19,7 @@ void sentinel::Photoeye::update(double dt_s, bool part_physically_present)
     else
     {
         m_time_in_pending_state_s += dt_s;
-        
+
         if (m_time_in_pending_state_s >= m_debounce_time_s && m_reported_state != m_pending_state)
         {
             m_reported_state = m_pending_state;
