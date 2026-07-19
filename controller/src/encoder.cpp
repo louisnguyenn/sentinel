c16ab239221b87cf2c19efc223cf93b425c796ec
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-19 15:00:06 -0400

    implement encoder update function

--- controller/src/encoder.cpp
+++ controller/src/encoder.cpp
@@ -1,7 +1,18 @@
 #include "encoder.hpp"
 
+/// @brief update the encoder with the speed of the conveyor belt through pulses
+/// @param dt_s
+/// @param conveyor_speed_mm_s
 void sentinel::Encoder::update(double dt_s, double conveyor_speed_mm_s)
 {
+    distance_this_tick_mm = conveyor_speed_mm_s * dt_s;       // calculate the distance from speed * time
+    pulses_this_tick = distance_this_tick * m_pulses_per_mm_; // calculate pulses each tick
+
+    // round down when counting pulses at low speeds - accumulate fractional part for accuracy
+    m_fractional_pulse_accumulator_ += pulses_this_tick;
+    whole_pulses = floor(m_fractional_pulse_accumulator_); // round down
+    m_pulse_count += whole_pulses;
+    m_fractional_pulse_accumulator_ -= whole_pulses;
 }
 
 uint32_t sentinel::Encoder::pulseCount() const
