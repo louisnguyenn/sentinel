17832522de19d71d055522f5fb07404eb601a879
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-19 15:03:27 -0400

    fix variable types and conversion

--- controller/src/encoder.cpp
+++ controller/src/encoder.cpp
@@ -1,17 +1,19 @@
 #include "encoder.hpp"
 
+#include <cmath>
+
 /// @brief update the encoder with the speed of the conveyor belt through pulses
 /// @param dt_s
 /// @param conveyor_speed_mm_s
 void sentinel::Encoder::update(double dt_s, double conveyor_speed_mm_s)
 {
-    distance_this_tick_mm = conveyor_speed_mm_s * dt_s;       // calculate the distance from speed * time
-    pulses_this_tick = distance_this_tick * m_pulses_per_mm_; // calculate pulses each tick
+    double distance_this_tick_mm = conveyor_speed_mm_s * dt_s;          // calculate the distance from speed * time
+    double pulses_this_tick = distance_this_tick_mm * m_pulses_per_mm_; // calculate pulses each tick
 
     // round down when counting pulses at low speeds - accumulate fractional part for accuracy
     m_fractional_pulse_accumulator_ += pulses_this_tick;
-    whole_pulses = floor(m_fractional_pulse_accumulator_); // round down
-    m_pulse_count += whole_pulses;
+    double whole_pulses = std::floor(m_fractional_pulse_accumulator_); // round down
+    m_pulse_count += static_cast<uint32_t> whole_pulses;
     m_fractional_pulse_accumulator_ -= whole_pulses;
 }
 
