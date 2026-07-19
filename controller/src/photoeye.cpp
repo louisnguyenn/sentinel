55fce09d5b25305d54aae1aeb4b36e432c9ef067
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-19 14:50:11 -0400

    implement photoeye state updated function

--- controller/src/photoeye.cpp
+++ controller/src/photoeye.cpp
@@ -1,13 +1,29 @@
 #include "photoeye.hpp"
 
-/// Advance the simulated sensor by dt_s, given the *true* physical
-/// state (whether a part is physically breaking the beam right now).
+/// @brief state of the photoeye sensor (HIGH if object, LOW if not object present)
+/// @param dt_s 
+/// @param part_physically_present 
 void sentinel::Photoeye::update(double dt_s, bool part_physically_present)
 {
-
+    if (part_physically_present != m_pending_state_)
+    {
+        // restart debounce timer
+        m_pending_state_ = part_physically_present;
+        m_time_in_pending_state_s_ = 0.0;
+    }
+    else
+    {
+        m_time_in_pending_state_s_ += dt_s;
+        
+        if (m_time_in_pending_state_s_ >= m_debounce_time_s_ && m_reported_state_ != m_pending_state_)
+        {
+            m_reported_state_ = m_pending_state_;
+        }
+    }
 }
 
-/// Debounced, reportable state.
+/// @brief return reported state
+/// @return private boolean
 bool sentinel::Photoeye::isBlocked() const
 {
     return m_reported_state_;
