1c60c3aacc6630e515fac07b1879edfab7f96771
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-19 15:00:25 -0400

    return pulse count

--- controller/src/encoder.cpp
+++ controller/src/encoder.cpp
@@ -17,6 +17,7 @@ void sentinel::Encoder::update(double dt_s, double conveyor_speed_mm_s)
 
 uint32_t sentinel::Encoder::pulseCount() const
 {
+    return m_pulse_count;
 }
 
 /// Distance implied by the pulse count, in mm.
