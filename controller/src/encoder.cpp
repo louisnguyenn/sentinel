18d2d0900a35a4c171b76385742c11e844b0a8ac
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-19 14:53:02 -0400

    create function skeleton

---
+++ controller/src/encoder.cpp
@@ -0,0 +1,18 @@
+#include "encoder.hpp"
+
+void sentinel::Encoder::update(double dt_s, double conveyor_speed_mm_s)
+{
+}
+
+uint32_t sentinel::Encoder::pulseCount() const
+{
+}
+
+/// Distance implied by the pulse count, in mm.
+double sentinel::Encoder::distanceMm() const
+{
+}
+
+void sentinel::Encoder::reset()
+{
+}
