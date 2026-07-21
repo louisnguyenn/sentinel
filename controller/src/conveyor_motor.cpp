5f477d26021f45cd5ae78b15911a637ca70121e4
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-21 18:11:17 -0400

    build function skeleton

---
+++ controller/src/conveyor_motor.cpp
@@ -0,0 +1,20 @@
+#include "conveyor_motor.hpp"
+
+sentinel::ConveyorMotor::ConveyorMotor(double rated_speed_mm_s, double accel_mm_s2)
+{
+}
+
+void sentinel::ConveyorMotor::setRunning(bool run)
+{
+}
+void sentinel::ConveyorMotor::update(double dt_s)
+{
+}
+
+double sentinel::ConveyorMotor::currentSpeedMmS() const
+{
+}
+
+bool sentinel::ConveyorMotor::isRunning() const
+{
+}
