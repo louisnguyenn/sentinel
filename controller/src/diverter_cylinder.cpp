06818c9502cbb58bc7fe395852331e1f69881e2f
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-21 18:30:11 -0400

    implement update

--- controller/src/diverter_cylinder.cpp
+++ controller/src/diverter_cylinder.cpp
@@ -11,14 +11,39 @@ void sentinel::DiverterCylinder::command(bool extend)
 
 void sentinel::DiverterCylinder::update(double dt_s)
 {
+    int direction{};
+
+    if (m_commanded_extend == true)
+    {
+        direction = 1; // extended
+    }
+    else
+    {
+        direction = -1; // retracted
+    }
+
+    double fraction_per_second = 1.0 / m_travel_time_s;
+
+    m_position_fraction += direction * fraction_per_second * dt_s; // calculate position
+
+    if (m_position_fraction <= 0.0)
+    {
+        m_position = Position::RETRACTED;
+    }
+    else if (m_position_fraction >= 1.0)
+    {
+        m_position = Position::EXTENDED;
+    }
+    else
+    {
+        m_commanded_extend ? m_position = Position::EXTENDING : m_position = Position::RETRACTING;
+    }
 }
 
 sentinel::DiverterCylinder::Position sentinel::DiverterCylinder::position() const
 {
 }
 
-/// 0.0 = fully retracted, 1.0 = fully extended — feed this into
-/// ProximitySensor::update().
 double sentinel::DiverterCylinder::positionFraction() const
 {
 }
