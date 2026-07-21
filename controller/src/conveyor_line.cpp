6abfb079745c76459ce0bfb0d789fcd1e4df09d9
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-21 18:58:47 -0400

    build function skeletons

---
+++ controller/src/conveyor_line.cpp
@@ -0,0 +1,33 @@
+#include "conveyor_line.hpp"
+
+void sentinel::ConveyorLine::update(double dt_s)
+{
+}
+
+void sentinel::ConveyorLine::spawnPart()
+{
+}
+
+bool sentinel::ConveyorLine::photoeyeBlocked() const
+{
+}
+
+uint32_t sentinel::ConveyorLine::encoderCount() const
+{
+}
+
+bool sentinel::ConveyorLine::diverterExtended() const
+{
+}
+
+bool sentinel::ConveyorLine::diverterRetracted() const
+{
+}
+
+void sentinel::ConveyorLine::commandDiverter(bool extend)
+{
+}
+
+void sentinel::ConveyorLine::setConveyorRunning(bool run)
+{
+}
