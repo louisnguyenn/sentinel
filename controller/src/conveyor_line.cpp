c474730f9efddbf841569caf23e8305b609758e0
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-25 14:49:55 -0400

    add docs to each update method calls

--- controller/src/conveyor_line.cpp
+++ controller/src/conveyor_line.cpp
@@ -15,8 +15,10 @@ void sentinel::ConveyorLine::update(double dt_s)
 
     for (size_t i{0}; i < m_parts.size();)
     {
+        // check if parts are past the line
         if (m_parts[i].position_mm > kLineLengthMm)
         {
+            // remove the part (they've left the line)
             m_parts.erase(m_parts.begin() + i);
         }
         else
@@ -25,10 +27,11 @@ void sentinel::ConveyorLine::update(double dt_s)
         }
     }
 
-    m_photoeye.update(dt_s, anyPartInInspectionZone());
-    m_encoder.update(dt_s, m_motor.currentSpeedMmS());
-    m_diverter.update(dt_s);
-    m_proximity.update(m_diverter.positionFraction());
+    // update components of the line
+    m_photoeye.update(dt_s, anyPartInInspectionZone()); // check if part is physically in the inspection zone
+    m_encoder.update(dt_s, m_motor.currentSpeedMmS());  // count how far the belt has moved using pulses
+    m_diverter.update(dt_s);    //  time speed accurately to divert the defected part
+    m_proximity.update(m_diverter.positionFraction());  // check if the arm has fully extended or not (stuck on extending)
 }
 
 void sentinel::ConveyorLine::spawnPart()
