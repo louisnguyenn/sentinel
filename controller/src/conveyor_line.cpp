38100ec427463440ae09b9dcf429e6c4a295a31b
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-25 14:13:18 -0400

    update components of conveyor line

--- controller/src/conveyor_line.cpp
+++ controller/src/conveyor_line.cpp
@@ -2,6 +2,30 @@
 
 void sentinel::ConveyorLine::update(double dt_s)
 {
+    m_motor.update(dt_s);
+    int i{};
+
+    for (auto &part : m_parts)
+    {
+        part.position_mm += m_motor.currentSpeedMmS() * dt_s;
+    }
+
+    for (size_t i{0}; i < m_parts.size();)
+    {
+        if (m_parts[i].position_mm > kLineLengthMm)
+        {
+            m_parts.erase(m_parts.begin() + i);
+        }
+        else
+        {
+            ++i;
+        }
+    }
+
+    m_photoeye.update(dt_s, anyPartInInspectionZone());
+    m_encoder.update(dt_s, m_motor.currentSpeedMmS());
+    m_diverter.update(dt_s);
+    m_proximity.update(m_diverter.positionFraction());
 }
 
 void sentinel::ConveyorLine::spawnPart()
