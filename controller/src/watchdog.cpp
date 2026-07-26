313265b44b11c8dc6116a161dbd9db855a89e746
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-26 14:25:02 -0400

    implement feed method

--- controller/src/watchdog.cpp
+++ controller/src/watchdog.cpp
@@ -5,17 +5,20 @@ sentinel::Watchdog::Watchdog(std::chrono::milliseconds timeout) : m_timeout{time
 {
 }
 
+// if heartbeat value is the same as last seen heartbeat value, do nothing; still waiting
 void sentinel::Watchdog::feed(uint16_t heartbeat_value)
 {
-
+    if (heartbeat_value != m_last_seen)
+    {
+        m_last_seen = heartbeat_value;
+        m_last_change = std::chrono::steady_clock::now();
+    }
 }
 
 bool sentinel::Watchdog::timedOut() const
 {
-
 }
 
 void sentinel::Watchdog::reset()
 {
-
 }
