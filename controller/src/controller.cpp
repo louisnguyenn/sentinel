c27fb4f69979475dafd73d44c24f225ffe50dd39
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-26 14:35:09 -0400

    build method skeleton

---
+++ controller/src/controller.cpp
@@ -0,0 +1,71 @@
+#include "controller.hpp"
+
+// public methods
+sentinel::Controller::Controller(ConveyorLine &line) : m_line{line}
+{
+}
+
+void sentinel::Controller::tick(double dt_s)
+{
+}
+
+void sentinel::Controller::setEstop(bool active)
+{
+}
+
+void sentinel::Controller::requestFaultReset()
+{
+}
+
+void sentinel::Controller::setMode(OperatingMode mode)
+{
+}
+
+void sentinel::Controller::submitInspectionResult(bool defective)
+{
+}
+
+void sentinel::Controller::feedVisionHeartbeat(uint16_t heartbeat_value)
+{
+}
+
+sentinel::CycleState sentinel::Controller::state() const
+{
+}
+
+sentinel::OperatingMode sentinel::Controller::mode() const
+{
+}
+
+sentinel::FaultCode sentinel::Controller::activeFault() const
+{
+}
+
+const sentinel::Stats sentinel::Controller::&stats() const
+{
+}
+
+// private methods
+void sentinel::Controller::inputScan()
+{
+}
+
+void sentinel::Controller::logicSolve()
+{
+}
+
+void sentinel::Controller::outputScan()
+{
+}
+
+void sentinel::Controller::housekeeping()
+{
+}
+
+void sentinel::Controller::enterFault(FaultCode code)
+{
+}
+
+void sentinel::Controller::attemptFaultReset()
+{
+}
