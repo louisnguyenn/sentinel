b60e2d65edc41eb8ef1f757961d13121f0871ad3
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-31 17:43:42 -0400

    feat: add test harness

---
+++ controller/tests/test_hardware.cpp
@@ -0,0 +1,23 @@
+#include "watchdog.hpp"
+#include <gtest/gtest.h>
+#include <thread>
+
+TEST(WatchdogTest, DoesNotTimeOutImmediatelyAfterFeed)
+{
+    sentinel::Watchdog wd(std::chrono::milliseconds(50));
+    wd.feed(1);
+    EXPECT_FALSE(wd.timedOut());
+}
+
+TEST(WatchdogTest, TimesOutAfterNoNewHeartbeat)
+{
+    sentinel::Watchdog wd(std::chrono::milliseconds(50));
+    wd.feed(1);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    EXPECT_TRUE(wd.timedOut());
+}
+
+// TODO:
+// - ResetClearsTimeoutCondition
+// - FeedingSameValueRepeatedlyEventuallyTimesOut
+//   (i.e. calling feed(1), feed(1), feed(1) shouldn't count as "new" heartbeats)
