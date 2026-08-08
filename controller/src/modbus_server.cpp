b370eedc69d35323d2b92c177de6b5a704598a1e
Louis Nguyen <louis.nguyen550@gmail.com>
2026-08-08 14:48:53 -0400

    feat: create function stubs for modbus implementations

---
+++ controller/src/modbus_server.cpp
@@ -0,0 +1,14 @@
+#include "modbus_server.hpp"
+#include <stdexcept>
+
+sentinel::ModbusServer::ModbusServer(const char* ip, int port)
+{
+}
+
+void sentinel::ModbusServer::poll()
+{
+}
+
+uint16_t* sentinel::ModbusServer::registers()
+{
+}
